2486c62390468e30050a10aa32283315695d910e
Chris_Aecom <chris.huang1@aecom.com>
2025-04-17 15:33:48 +0800

    com

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from routers import slpkRouter  # Import the routers
+
+
+import argparse
+
+
 app = FastAPI()
 # Include the router
 app.include_router(slpkRouter.router)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", help="debug mode",
+                        action='store_true', required=False)
+    args = parser.parse_args()
+    debugger_mode = args.d
     import uvicorn
-    uvicorn.run(r"main:app", host="localhost",
+    uvicorn.run(r"main:app", host="localhost", reload=debugger_mode,
                 port=8000, workers=8)
+    pass
